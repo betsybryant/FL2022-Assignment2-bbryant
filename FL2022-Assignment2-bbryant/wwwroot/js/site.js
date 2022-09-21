d7a0728d2c778aa7486eacb0052528790fce0fcc
Betsy Bryant <embryant1@crimson.ua.edu>
2022-09-20 21:13:34 -0500

    Add project files.

---
+++ FL2022-Assignment2-bbryant/wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿var len;
+var results = '';
+
+function apiSearch() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "d3898977df684e5e9ea3679a46c10102");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            for (i = 0; i < len; i++) {
+                results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+            }
+
+            $('#searchResults').html(results);
+            $('#searchResults').dialog();
+        })
+        .fail(function () {
+            alert("error");
+        });
+}
+function GetDate() {
+    console.log("HELLO");
+    date = new Date();
+    hour = date.getHours();
+    minute = date.getMinutes();
+    document.getElementById("time").innerHTML = hour + ":" + minute;
+}
+
+function ChangeImage() {
+
+    document.getElementById("imageChange").src = "https://images.unsplash.com/photo-1663687328323-3e0f69873e1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80";
+
+}
